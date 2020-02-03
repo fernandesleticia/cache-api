319ba35fcf6d29126967618f7cea5fd210b59fa9
fernandesleticia <leticiafernandes777@gmail.com>
2020-02-03 11:32:08 -0300

    add seed for dummy transaction

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+    Transaction.create({
+        date: Faker::Date.in_date_period,
+        description: Faker::Lorem.paragraph,
+        reference: Faker::Number.number(digits: 10),
+        withdrawal:  Faker::Number.number(100000),
+        deposit:  Faker::Number.number(100000) 
+    })   
+end
